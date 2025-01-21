38fc36b801c8444cf7876242a52762ca28f91007
WEREWOLF-DEMON <153902241+WEREWOLF-DEMON@users.noreply.github.com>
2025-01-21 17:51:48 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "6435225"
+# -------------------------------------------------------------
+API_HASH = "4e984ea35f854762dcde906dce426c2d"
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+STRING1 = getenv("STRING_SESSION", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = int(getenv("OWNER_ID", "6656608288"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/yourtoofan/CHATBOT")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+SUPPORT_GRP = "kaalCarder"
+UPDATE_CHNL = "SweetToxicUpdate"
+OWNER_USERNAME = "WerewolfDemonInfo"
+# GIT TOKEN ( if your edited repo is private)
+GIT_TOKEN = getenv("GIT_TOKEN", "")
+    
